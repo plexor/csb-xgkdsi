f0f0b5552e93a917f44e87a9e69e0902f91e7575
Code Sandbox <hello@codesandbox.io>
2022-03-05 21:41:40 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.2ed6f2ce44eb209991c09284d7d62a54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1416e1772c48dc97ebc83289a64945ed",
+    "url": "https://plexor.github.io/csb-xgkdsi/index.html"
+  },
+  {
+    "revision": "4bf46851007a9127813e",
+    "url": "https://plexor.github.io/csb-xgkdsi/static/css/main.1e39348c.chunk.css"
+  },
+  {
+    "revision": "d6a2ce4698cb0ea25880",
+    "url": "https://plexor.github.io/csb-xgkdsi/static/js/2.41dc12dc.chunk.js"
+  },
+  {
+    "revision": "7c3782a7e65f48d3b45b3ebb61610f23",
+    "url": "https://plexor.github.io/csb-xgkdsi/static/js/2.41dc12dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4bf46851007a9127813e",
+    "url": "https://plexor.github.io/csb-xgkdsi/static/js/main.ca3d7992.chunk.js"
+  },
+  {
+    "revision": "87ccf9c5b51d7c5bb393",
+    "url": "https://plexor.github.io/csb-xgkdsi/static/js/runtime-main.40bdadd4.js"
+  }
+]);
